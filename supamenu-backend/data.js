02fce7f092ff7124b92653e2a393d8dc34cfef18
b-sandrine <sanrdushimimana@gmail.com>
2023-05-19 11:41:23 +0200

    Added sample data

---
+++ supamenu-backend/data.js
@@ -0,0 +1,15 @@
+var suppliers = [ 
+    {id: 1, name: 'Nyirangarama'},
+    {id: 2, name: 'Mukamira'},
+    {id: 3, name: 'Inyange'}
+]
+
+var drinks = [
+    { id: 1, name: 'Agashya', type: 'Juice', ownerId: 1 },
+    { id: 2, name: 'Drinking Yoghurt', type: 'Milk', ownerId: 2},
+    { id: 3, name: 'Inyange water',type: 'Water', ownerId: 3 },
+    { id: 4, name: 'Akarabo', type: 'Biscuit', ownerId: 1 },
+    { id: 5, name: 'Akandi', type: 'Water', ownerId: 1},
+    { id: 6, name: 'Ikivuguto',type: 'Milk', ownerId: 2 },
+    { id: 7, name: 'Inyange Juice', type: 'Juice' , ownerId: 3 },
+]
