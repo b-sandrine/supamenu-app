69ba837b2327054af0fe684138af5a6454a84f8a
b-sandrine <sanrdushimimana@gmail.com>
2023-05-19 12:15:54 +0200

    Displaying data & ready to write queries

--- supamenu-backend/index.js
+++ supamenu-backend/index.js
@@ -1,22 +1,68 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
-const { buildSchema } = require('graphql')
+const { GraphQLObjectType, GraphQLNonNull, GraphQLInt, GraphQLString, GraphQLList, GraphQLSchema } = require('graphql')
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`);
+var Suppliers = [ 
+    {id: 1, name: 'Nyirangarama'},
+    {id: 2, name: 'Mukamira'},
+    {id: 3, name: 'Inyange'}
+]
 
-const root = {
-    hello: () => 'Hello, World!'
-};
+var Drinks = [
+    { id: 1, name: 'Agashya', type: 'Juice', ownerId: 1 },
+    { id: 2, name: 'Drinking Yoghurt', type: 'Milk', ownerId: 2},
+    { id: 3, name: 'Inyange water',type: 'Water', ownerId: 3 },
+    { id: 4, name: 'Akarabo', type: 'Biscuit', ownerId: 1 },
+    { id: 5, name: 'Akandi', type: 'Water', ownerId: 1},
+    { id: 6, name: 'Ikivuguto',type: 'Milk', ownerId: 2 },
+    { id: 7, name: 'Inyange Juice', type: 'Juice' , ownerId: 3 },
+]
+
+const suppliersType = new GraphQLObjectType({
+    name: 'Suppliers',
+    description: 'The products manufactures and suppliers',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt)},
+        name: { type: GraphQLNonNull(GraphQLString)},
+    })
+})
+
+const drinksType = new GraphQLObjectType({
+    name: 'Drinks',
+    description: 'The products manufactured and suppliers some info',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt)},
+        name: { type: GraphQLNonNull(GraphQLString)},
+        type: { type: GraphQLNonNull(GraphQLString)},
+        ownerId: { type: GraphQLNonNull(GraphQLString)},
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: "Root Query",
+    fields: () => ({
+        Suppliers : {
+            type: new GraphQLList(suppliersType),
+            description: 'List of all manufactures',
+            resolve: () => Suppliers
+        },
+        Drinks: {
+            type: new GraphQLList(drinksType),
+            description: 'List of all drinks',
+            resolve: () => Drinks
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+})
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true
 }));
 
