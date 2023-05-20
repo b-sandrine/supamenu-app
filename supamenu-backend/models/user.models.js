780b23dd890ad5d81964f96e238e2af9e9f213c0
b-sandrine <sanrdushimimana@gmail.com>
2023-05-20 08:32:40 +0200

    Added user model

--- supamenu-backend/models/user.models.js
+++ supamenu-backend/models/user.models.js
@@ -1,1 +1,11 @@
-console.log('user models')
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const User = mongoose.model('Users', userSchema)
+
+module.exports = User;
