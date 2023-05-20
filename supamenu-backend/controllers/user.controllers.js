9190f44476ad79152f484d018a7fa9b80d0eac7b
b-sandrine <sanrdushimimana@gmail.com>
2023-05-20 08:56:06 +0200

    Added controllers

--- supamenu-backend/controllers/user.controllers.js
+++ supamenu-backend/controllers/user.controllers.js
@@ -1,1 +1,15 @@
-console.log('User controllers')
+import User from '../models/user.models'
+
+const userController = {
+    getAllUsers: async () => {
+        try {
+            const users = await User.find();
+            return users;
+        }
+        catch (error) {
+            throw new Error('Failed to fetch users')
+        }
+    }
+}
+
+module.exports = userController;
