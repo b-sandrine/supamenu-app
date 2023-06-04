b9dd66eb0a2b6aed4c9ba69cb4935b75399fc883
b-sandrine <sanrdushimimana@gmail.com>
2023-06-04 09:11:00 +0200

    created sidebar

--- supamenu-web/src/components/sidebar/sidebar.tsx
+++ supamenu-web/src/components/sidebar/sidebar.tsx
@@ -6,24 +6,26 @@ const sidebar = () => {
       <h3>1. Create your restaurant profile</h3>
       <div className="sample--elements">
         <div className="single--element">
-          <div className="number"><p>1</p></div>
+          <div className="number">1</div>
           <div className="details">
             <h5>Restaurant Information</h5>
             <p>Restaurant name, address, Details, owner details</p>
           </div>
         </div>
+
         <div className="single--element">
-          <div className="number"><p>2</p></div>
+          <div className="number">2</div>
           <div className="details">
             <h5>Restaurant Type & Timings</h5>
             <p>Establishment & cuisine type, opening hours</p>
           </div>
         </div>
+
         <div className="single--element">
-          <div className="number"><p>3</p></div>
+          <div className="number">3</div>
           <div className="details">
             <h5>Create your menu</h5>
-            <p>Menu, restaurant, food images</p>
+            <p>Menu, restaurant, food images </p>
           </div>
         </div>
       </div>
