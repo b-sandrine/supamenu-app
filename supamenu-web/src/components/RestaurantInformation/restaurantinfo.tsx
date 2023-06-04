b74bad32caed4ca8e434afdc96870f3c97a195b8
b-sandrine <sanrdushimimana@gmail.com>
2023-06-04 19:31:37 +0200

    added page 1

---
+++ supamenu-web/src/components/RestaurantInformation/restaurantinfo.tsx
@@ -0,0 +1,40 @@
+import "./restaurantinfo.css";
+const restaurantinfo = () => {
+  return (
+    <div className="restuarantinfo--container">
+      <div className="form">
+        <div className="form--component">
+          <label>Restaurant Information</label>
+          <input type="text" name="" id="" placeholder="Restaurant Name" />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Restaurant Complete Name"
+          />
+        </div>
+        <div className="form--component">
+          <label>Contact number @Restaurant</label>
+          <input type="text" name="" id="" placeholder="Mobile number" />
+        </div>
+
+        <div className="form--component">
+          <label>Restaurant owner details</label>
+          <input type="text" name="" id="" placeholder="Mobile number" />
+          <div className="flex">
+            <input type="text" name="" id="" placeholder="Owner Name" style={{marginRight: '5%'}} />
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Restaurant Owner Email"
+              style={{marginLeft: '5%'}} 
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default restaurantinfo;
--- supamenu-web/src/pages/settingup/settingup.tsx
+++ supamenu-web/src/pages/settingup/settingup.tsx
@@ -1,14 +1,27 @@
-import './settingup.css'
-import Navbar from '../../components/navbar/navbar';
-import Sidebar from '../../components/sidebar/sidebar';
+import "./settingup.css";
+import Navbar from "../../components/navbar/navbar";
+import Sidebar from "../../components/sidebar/sidebar";
+import Restaurantinfo from "../../components/RestaurantInformation/restaurantinfo";
+import {FaAngleLeft, FaAngleRight} from 'react-icons/fa'
 
 const settingup = () => {
-    return (
-        <div className="settingup--container">
-            <Navbar />
-            <Sidebar />
-        </div>
-    )
-}
+  return (
+    <div className="settingup--container">
+      <Navbar />
+      <Sidebar />
+      <Restaurantinfo />
+      <div className="footer">
+        <button className="left">
+          <FaAngleLeft />
+          <p>Back</p>
+        </button>
+        <button className="right">
+          <p>Next</p>
+          <FaAngleRight />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default settingup;
